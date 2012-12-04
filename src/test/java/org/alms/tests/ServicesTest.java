720cc9496dfbf63462d5383bd5c50c3927208929
Scott Ross <sr523@cornell.edu>
2012-12-04 13:12:51 -0500

    GNU LESSER GENERAL PUBLIC LICENSE

--- src/test/java/org/alms/tests/ServicesTest.java
+++ src/test/java/org/alms/tests/ServicesTest.java
@@ -1,4 +1,13 @@
-
+/*******************************************************************************
+ * Copyright (c) 2012 Scott Ross.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Scott Ross - initial API and implementation
+ ******************************************************************************/
 package org.alms.tests;
 
 import static org.junit.Assert.*;
